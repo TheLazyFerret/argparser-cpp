c7e13c2a40dbae78e286660b783f656e1abf9a59
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-09-29 10:42:38 +0000

    add argparser.hpp file

---
+++ include/argparser.hpp
@@ -0,0 +1,6 @@
+//! Author TheLazyFerret (https://github.com/TheLazyFerret)
+//! Copyright (c) 2025 TheLazyFerret
+//!   Using the license MIT. 
+//!   See the LICENSE file in the project root for full license information.
+//!
+//! 
